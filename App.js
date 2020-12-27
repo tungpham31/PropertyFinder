1b78ac12be90bc9123fbdb921c0dc41c29c83a88
Steven Pham <tungpham@fb.com>
2020-12-26 23:35:51 -0800

    First commit

---
+++ App.js
@@ -0,0 +1,116 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+'use strict';
+
+import React, {useState} from 'react';
+//import 'react-native-gesture-handler';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+  TextInput,
+  Image,
+  Button,
+} from 'react-native';
+
+import {
+  Header,
+  LearnMoreLinks,
+  Colors,
+  DebugInstructions,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+
+const Stack = createStackNavigator();
+
+const SearchPage = () => {
+  const [searchText, setSearchText] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+  let textInput;
+
+  const onTextInputSubmitted = (textInput) => {
+    console.log(searchText);
+    textInput.clear();
+  }
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.description}>
+        Search for houses to buy!
+      </Text>
+      <Text style={styles.description}>
+        Search by place-name or postal code
+      </Text>
+      <View style={styles.searchBarContainer}>
+        <TextInput
+          style={styles.textInput}
+          returnKeyType="go"
+          placeholder="Search via name or postal code"
+          ref={input => { textInput = input }}
+          onChangeText = {text => setSearchText(text)}
+          onSubmitEditing={() => onTextInputSubmitted(textInput)}
+        />
+        <Button
+          title="Go"
+          color="#48BBEC"
+          onPress={() => onTextInputSubmitted(textInput)}
+        />
+      </View>
+      <Image source={require('./Resources/house.png')} style={styles.image}/>
+    </View>
+  );
+}
+
+const App: () => React$Node = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Home"
+          component={SearchPage}
+          options={{ title: 'Property Finder' }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+    );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+  description: {
+    fontSize: 18,
+    textAlign: 'center',
+    color: '#656565',
+    marginTop: 20,
+  },
+  searchBarContainer: {
+    flexDirection: 'row',
+    marginTop: 20,
+  },
+  textInput: {
+    width: 320,
+    height: 40,
+    borderColor: '#48BBEC',
+    borderWidth: 1,
+    borderRadius: 8,
+    marginRight: 5,
+  }
+});
+
+export default App;
